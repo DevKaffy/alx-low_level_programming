671a99c3566688617ccd38e41b894885b39fa3e8
ADEWUMI, KAFILAT ENIOLA <kafilatadewumi61@gmail.com>
2022-09-09 17:13:46 -0700

    print the alphabet in lowercase except q and e, followed by a new line

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,23 @@
+#include<unistd.h>
+
+/**
+ * main - print the letters of the alphabet except e and q
+ *
+ * Return: always returns 0
+ *
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'q' || a == 'e')
+		{
+			continue;
+		}
+		putchar(a);
+	}
+	putchar('\n');
+	return(0);
+}
