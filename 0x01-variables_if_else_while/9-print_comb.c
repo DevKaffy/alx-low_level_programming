e9672c76190d74db7286fbb2d82e952484e3063b
ADEWUMI, KAFILAT ENIOLA <kafilatadewumi61@gmail.com>
2022-09-09 18:22:24 -0700

    print all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print comibnation of single digits numbers
+ *
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 10; a < 20; a++)
+	{
+		putchar((a % 10) + '0');
+		if (a != 19)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
