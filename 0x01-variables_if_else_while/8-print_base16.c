bc31126e48da50adeb85c7004f988b5a3c605e9b
ADEWUMI, KAFILAT ENIOLA <kafilatadewumi61@gmail.com>
2022-09-09 18:09:12 -0700

    prints all the numbers of base 16 in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers of base16 in lowercase
+ *
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	unsigned char a = '0';
+	int z;
+
+	for (z = 0; z < 10; z++)
+	{
+		putchar(a);
+		a++;
+	}
+	a = '1';
+	for (z = 0; z < 6; z++)
+	{
+		putchar('0' + a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
+		
