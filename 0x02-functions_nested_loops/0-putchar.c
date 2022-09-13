5dbb4452e1eb0a6ed131935e47f27edd6b1d9db2
ADEWUMI, KAFILAT ENIOLA <kafilatadewumi61@gmail.com>
2022-09-13 07:00:21 -0700

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-	char str[] = "_putchar";
-	int i;
-
-	for (i = 1; i <= 8; ++i)
-	{
-		str[i];
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
