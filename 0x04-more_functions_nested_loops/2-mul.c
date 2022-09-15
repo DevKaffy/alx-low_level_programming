41c97978fb4ba6a866c0cb38d0eb85bea007bfa5
ADEWUMI, KAFILAT ENIOLA <kafilatadewumi61@gmail.com>
2022-09-15 13:46:25 -0700

    multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiply two integers
+ * @a: character compare
+ * @b: character compare
+ * Result: result of a and b
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b)
+}
