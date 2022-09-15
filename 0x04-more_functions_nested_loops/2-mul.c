02f7f032ae25b3c7139b2bfdfbc449054bc0a2ae
ADEWUMI, KAFILAT ENIOLA <kafilatadewumi61@gmail.com>
2022-09-15 13:50:00 -0700

    update

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
  * mul - multiply two integers
- * @a: character compare
- * @b: character compare
- * Result: result of a and b
- *
+ * @a: int to mult
+ * @b: int to mult
+ * Return: result of a and b
  */
 
 int mul(int a, int b)
