c146ce9acec1f64e15a6965851b54b92e425ecb6
ADEWUMI, KAFILAT ENIOLA <kafilatadewumi61@gmail.com>
2022-09-15 16:05:38 -0700

    find and print the largest prime factors of the number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	unsigned int i = 2;
+	unsigned long j = 612852475143;
+
+	while (a != n)
+	{
+		if (n % a == 0)
+		{
+			n = n / a;
+		} else
+		{
+			a++;
+		}
+	}
+	printf("%lu\n", n);
+	return (0);
+}
