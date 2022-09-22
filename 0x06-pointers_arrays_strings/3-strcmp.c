d85b2dd5973004f30c2dc6ce5a57f69761f71301
ADEWUMI, KAFILAT ENIOLA <kafilatadewumi61@gmail.com>
2022-09-22 03:39:21 -0700

    fixes

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,8 +5,8 @@
  * @s1: pointer to the first string to be compared.
  * @s2: pointer to the second string to be compared.
  * Return: If str1 < str2, d negative diff of the first unmatched characters.
- * 	If srt1 == str2, 0
- * 	If str1 > str2, d positive diff of the first unmatched characters.
+ * If srt1 == str2, 0
+ * If str1 > str2, d positive diff of the first unmatched characters.
  */
 int _strcmp(char *s1, char *s2)
 {
