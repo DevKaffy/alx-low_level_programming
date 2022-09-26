403692cf279b47fb2441a8aa104c95e90bda52cd
ADEWUMI, KAFILAT ENIOLA <kafilatadewumi61@gmail.com>
2022-09-26 14:26:23 -0700

    fixes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,11 +12,14 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
 
-	for (i = 0; *(s + i) != '\0'; j++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		if (*(s + i) == *(accept + j))
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			return (s + i);
+			if (*(s + i) == *(accept + j))
+			{
+				return (s + i);
+			}
 		}
 	}
 }
