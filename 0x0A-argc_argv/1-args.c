4d1f98ab0e8448fcf0944ff59cf884a30d061a28
ADEWUMI, KAFILAT ENIOLA <kafilatadewumi61@gmail.com>
2022-09-30 08:17:45 -0700

    fixes

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,7 +3,7 @@
  * main - prints a number, followed by a new line
  * @argc: number of the arguments supplied to a program
  * @argv: array of pointers to the arguments
- * Result: Always 0
+ * Return: Always 0
  */
 int main(int argc, char  __attribute__((__unused__)) *argv[])
 {
