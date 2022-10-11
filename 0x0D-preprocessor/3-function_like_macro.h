739d99f0f81c2f37cafbc6685a9fb0be10896a9f
ADEWUMI, KAFILAT ENIOLA <kafilatadewumi61@gmail.com>
2022-10-11 07:57:36 -0700

    fixes

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef ABS_H
+#define ABS_H
+
 /**
  * File: 3-function_like_macro.h
  * Desc: Header file that defines a function-like macro ABS(x)
